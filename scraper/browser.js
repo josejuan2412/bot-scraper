02e232e1aad7e73c67a5e0c99fc51cb6ec9d08e8
Jose J. Cruz <jjzcru@gmail.com>
2022-10-03 10:53:57 -0500

    feat: :sparkles: Support for chromium executable path

--- scraper/browser.js
+++ scraper/browser.js
@@ -1,30 +1,35 @@
-const chromium = require("chrome-aws-lambda");
-require("dotenv/config");
+const chromium = require('chrome-aws-lambda');
+require('dotenv/config');
 
 class Browser {
-  constructor(browser) {
-    this.browser = browser;
-  }
-  async getPage(product) {
-    const url = `https://smile.amazon.com/dp/${product}?aod=1`;
-    let page = await this.browser.newPage();
+	constructor(browser) {
+		this.browser = browser;
+	}
+	async getPage(product) {
+		const url = `https://smile.amazon.com/dp/${product}?aod=1`;
+		let page = await this.browser.newPage();
 
-    console.log(`AFTER PAGE`);
-    await page.goto(url, { waitUntil: "load" });
-    console.log(`AFTER PAGE GOTO`);
-    return page;
-  }
+		console.log(`AFTER PAGE`);
+		await page.goto(url, { waitUntil: 'load' });
+		console.log(`AFTER PAGE GOTO`);
+		return page;
+	}
 
-  static async build() {
-    return await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: "/opt/homebrew/bin/chromium", //await chromium.executablePath,
-      headless: chromium.headless,
-    });
-  }
+	static async build() {
+		const executablePath =
+			process.env.NODE_ENV === 'local'
+				? process.env.CHROMIUM_PATH
+				: await chromium.executablePath;
+
+		return await chromium.puppeteer.launch({
+			args: chromium.args,
+			defaultViewport: chromium.defaultViewport,
+			executablePath,
+			headless: chromium.headless,
+		});
+	}
 }
 
 module.exports = {
-  Browser,
+	Browser,
 };
