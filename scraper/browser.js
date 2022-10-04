1027f98448ee1b0c55f4456e222bcce901b6b7f7
jose juan jaen small <josejuan2412@JJJ-MacMini.local>
2022-10-03 19:47:21 -0500

    changes to tracking products

--- scraper/browser.js
+++ scraper/browser.js
@@ -8,7 +8,10 @@ class Browser {
     const url = `https://smile.amazon.com/dp/${product}?aod=1`;
     let page = await this.browser.newPage();
     console.log(`AFTER PAGE`);
-    await page.goto(url, { waitUntil: "load" });
+    await page.goto(url, {
+      waitUntil: ["load", "networkidle0", "networkidle2"],
+      timeout: 0,
+    });
     console.log(`AFTER PAGE GOTO`);
     return page;
   }
@@ -18,9 +21,14 @@ class Browser {
       process.env.NODE_ENV === "local"
         ? process.env.CHROMIUM_PATH
         : await chromium.executablePath;
-
+    let args = [
+      ...chromium.args,
+      "--disable-features=AudioServiceOutOfProcess",
+      "--disable-gpu",
+      "--disable-software-rasterize",
+    ];
     return await chromium.puppeteer.launch({
-      args: chromium.args,
+      args: args,
       defaultViewport: chromium.defaultViewport,
       executablePath,
       headless: chromium.headless,
